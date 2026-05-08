b1272ebe71fd1bf954588f6f28a66be3928df747
Fernando196 <fer_jose1996@hotmail.com>
2026-05-07 23:19:36 -0500

    feat: common guards and decorators

---
+++ src/common/guards/roles.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const rolesRequeridos = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!rolesRequeridos) return true;
+
+    const { user } = context.switchToHttp().getRequest();
+    if (!rolesRequeridos.includes(user.rol)) {
+      throw new ForbiddenException('No tienes permisos para esta acción');
+    }
+    return true;
+  }
+}
