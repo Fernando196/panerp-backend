e718405e762aa4b2ca9218ccc2c271c876e7faf2
Fernando196 <fer_jose1996@hotmail.com>
2026-05-07 23:19:44 -0500

    feat: auth module

---
+++ src/app/strategies/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Sesion } from '@/domain/entitites/sesion.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    config: ConfigService,
+    @InjectRepository(Sesion) private sesionRepo: Repository<Sesion>,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    const sesion = await this.sesionRepo.findOne({ where: { id: payload.sid } });
+    if (!sesion || sesion.expira_at < new Date()) {
+      throw new UnauthorizedException('Sesión expirada');
+    }
+    return { id: payload.sub, rol: payload.rol, sessionId: payload.sid };
+  }
+}
